26925da7323722fadbaa866ffacc977e4cde87af
jungmoo23 <jjeonmu_guk@naver.com>
2022-10-29 16:33:19 +0900

    10/29

--- app/src/main/java/com/example/listview22/MainActivity.java
+++ app/src/main/java/com/example/listview22/MainActivity.java
@@ -3,44 +3,59 @@ package com.example.listview22;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList list ;
-
+    private String TAG = "MainActivity";
+    ArrayList<Pocker> pocker;
+    Pocker p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayListInit();
-        ListView listView = findViewById(R.id.main_list);
+    }
 
-        listAdaptor ap = new listAdaptor(this, R.layout.listview_main,list);
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        listInit();
+        ListView listView = (ListView)findViewById(R.id.main_list);
+
+        listAdaptor ap = new listAdaptor(this, R.layout.activity_main,pocker);
 
         listView.setAdapter(ap);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d(TAG, "onItemClick: "+i);
+                String nameAndWork = "이름:: "+pocker.get(i).getName()+" 배우:: "+pocker.get(i).getWork();
+               Toast.makeText(MainActivity.this, nameAndWork, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
-    private void ArrayListInit(){
-        list = new ArrayList();
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
-        list.add(new Pocker("학교","테스트","몰라"));
+    private void listInit(){
+        pocker = new ArrayList<Pocker>();
 
-
-
-
+        pocker.add(new Pocker("한효주","배우"));
+        pocker.add(new Pocker("강동호","배우"));
+        pocker.add(new Pocker("송강호","배우"));
+        pocker.add(new Pocker("최지민","배우"));
+        pocker.add(new Pocker("강수진","배우"));
+        pocker.add(new Pocker("송혜교","배우"));
 
     }
 
--- app/src/main/java/com/example/listview22/listAdaptor.java
+++ app/src/main/java/com/example/listview22/listAdaptor.java
@@ -6,7 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,39 +17,45 @@ import androidx.core.graphics.PointKt;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class listAdaptor extends ArrayAdapter {
-    public static String TAG = "listAdaptor";
 
-    ArrayList<Pocker> list ;
-    public listAdaptor(@NonNull Context context, int resource,ArrayList<Pocker> pp) {
-        super(context, resource);
-        list =pp;
+    private  ArrayList<Pocker>list;
+    private String TAG = "listAdaptor";
+    public listAdaptor(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+        this.list = (ArrayList<Pocker>) objects;
     }
-
 
     @Override
     public int getCount() {
-        Log.d(TAG, "getCount: "+list.size());
         return list.size();
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-
-        Log.d(TAG, "getView: ");
-
         View view = LayoutInflater.from(getContext()).inflate(R.layout.listview_main,parent,false);
 
-        Pocker po = (Pocker)list.get(position);
-        TextView t1 = view.findViewById(R.id.t1);
-        TextView t2 = view.findViewById(R.id.t2);
-        TextView t3 = view.findViewById(R.id.t3);
+        TextView t1 = view.findViewById(R.id.name);
+        TextView t2 = view.findViewById(R.id.work);
+        LinearLayout linearLayout = view.findViewById(R.id.name111);
 
-        t1.setText(po.getImage());
-        t2.setText(po.getName());
-        t3.setText(po.getWork());
+        Pocker pocker = list.get(position);
+
+        t1.setText(pocker.getName());
+        t2.setText(pocker.getWork());
+
+//        linearLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String name = "이름: "+t1.getText()+" 직업:: "+t2.getText();
+//                Toast toast = Toast.makeText(getContext(),name.toString(),Toast.LENGTH_SHORT);
+//                toast.show();
+//                Log.d(TAG, "onClick: "+t1.getText()+" "+t2.getText());
+//            }
+//        });
 
         return view;
     }
